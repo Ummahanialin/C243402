2ebf1aed404ba3b823a07b57e50f470f3ca19d89
Ummahanialin <alinefgh1234@gmail.com>
2026-03-22 20:54:10 +0600

    Add files via upload

---
+++ Linear search.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main() {
+int n, key;
+cout << "Enter array size: ";
+cin >> n;
+int arr[n];
+cout << "Enter " << n << " elements:\n";
+for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+}
+cout << "Enter value to search: ";
+cin >> key;
+int position = -1;
+for(int i = 0; i < n; i++) {
+if(arr[i] == key) {
+position = i;
+break;
+}
+}
+if(position != -1) {
+cout << "Element found at index: " << position << endl;
+} else {
+cout << "Element not found." << endl;
+}
+
+return 0;
+}
